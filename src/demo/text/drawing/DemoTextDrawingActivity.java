ae2965d0979a59a3da99bb468296c13274b6c93f
Ricky <ricky@kakao.com>
2013-01-18 12:44:12 +0900

    샘플 리소스 표현

--- src/demo/text/drawing/DemoTextDrawingActivity.java
+++ src/demo/text/drawing/DemoTextDrawingActivity.java
@@ -1,5 +1,9 @@
 package demo.text.drawing;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -7,7 +11,6 @@ import android.graphics.Rect;
 import android.os.Bundle;
 import android.text.StaticLayout;
 import android.text.TextPaint;
-import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
@@ -16,39 +19,43 @@ public class DemoTextDrawingActivity extends Activity {
 	
     private static final TextPaint TEXT_PAINT = new TextPaint();
     {
-    	TEXT_PAINT.setTextSize(30);
+    	TEXT_PAINT.setTextSize(24);
     	TEXT_PAINT.setAntiAlias(true);
     }
 	private static final LayoutParams LAYOUT_PARAMS = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, 1);
 	//private static final String LONG_STRING = "Please try this is a long text to fix into this view. Thanks!";
 	private static final String LONG_STRING = "Start\n안녕하세요! 다시 만나서 반가워요! 이쪽으로 오세요 안녕하세요! 다시 만나서 반가워요! 이쪽으로 오세요 안녕하세요! 다시 만나서 반가워요! 이쪽으로 오세요 안녕하세요! 다시 만나서 반가워요! 이쪽으로 오세요 안녕하세요! 다시 만나서 반가워요! 이쪽으로 오세요 안녕하세요! 다시 만나서 반가워요! 이쪽으로 오세요 안녕하세요! 다시 만나서 반가워요! 이쪽으로 오세요 안녕하세요! 다시 만나서 반가워요! 이쪽으로 오세요 안녕하세요! 다시 만나서 반가워요! 이쪽으로 오세요 안녕하세요! 다시 만나서 반가워요! 이쪽으로 오세요 ";
-
+     
 	/** Called when the activity is first created. */
-    @Override
+    @SuppressLint("DrawAllocation")
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        final String content;
+        
+        try {
+        	InputStream is = getAssets().open("1-1.txt");
+            
+            // We guarantee that the available method returns the total
+            // size of the asset...  of course, this does mean that a single
+            // asset can't be more than 2 gigs.
+            int size = is.available();
+            
+            // Read the entire asset into a local byte buffer.
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            
+         // Convert the buffer into a string.
+            content = new String(buffer);
+    	} catch (IOException e) {
+    		// Should never happen!
+    		throw new RuntimeException(e);
+    	}
+        
         LinearLayout linearLayout = new LinearLayout(this);
         linearLayout.setOrientation(LinearLayout.VERTICAL);
-        View view01 = new View(this){
-        	@Override
-        	protected void onDraw(Canvas canvas) {
-        		super.onDraw(canvas);
-        		TEXT_PAINT.setColor(Color.WHITE);
-        		//Drawing the text at middle of the view
-				canvas.drawText(LONG_STRING, 0, getHeight()/2, TEXT_PAINT);
-				TEXT_PAINT.setColor(Color.RED);
-				//Here we draw the middle line
-				canvas.drawLine(0, getHeight()/2, getWidth(), getHeight()/2, TEXT_PAINT);
-
-				Rect rect = new Rect();
-				TEXT_PAINT.setColor(Color.BLUE);
-				TEXT_PAINT.getTextBounds(LONG_STRING, 0, LONG_STRING.length(), rect);
-				//Measure the height and draw line on top of text
-				canvas.drawLine(0, getHeight()/2-rect.height(), getWidth(), getHeight()/2-rect.height(), TEXT_PAINT);
-        	}
-        };
-        view01.setBackgroundColor(Color.LTGRAY);
-		linearLayout.addView(view01, LAYOUT_PARAMS);
         
         View view02 = new View(this){
         	@Override
@@ -57,7 +64,7 @@ public class DemoTextDrawingActivity extends Activity {
         		TEXT_PAINT.setColor(Color.WHITE);
         		//Create a static layout to draw 
 				StaticLayout staticLayout = new StaticLayout(
-						LONG_STRING, 0, LONG_STRING.length(),
+						content, 0, content.length(),
 						TEXT_PAINT, getWidth(),android.text.Layout.Alignment.ALIGN_NORMAL , 
 						1.0f, 1.0f, false);
 				//staticLayout.draw(canvas);
@@ -85,7 +92,7 @@ public class DemoTextDrawingActivity extends Activity {
 					int start = staticLayout.getLineStart(i);
 					int end = staticLayout.getLineEnd(i);
 					
-					String lineString = LONG_STRING.substring(start, end);
+					String lineString = content.substring(start, end);
 					
 					TEXT_PAINT.setColor(Color.WHITE);
 					canvas.drawText(lineString, staticLayout.getLineLeft(i), staticLayout.getLineBottom(i), TEXT_PAINT);
@@ -95,29 +102,7 @@ public class DemoTextDrawingActivity extends Activity {
         };
         view02.setBackgroundColor(Color.DKGRAY);
 		linearLayout.addView(view02, LAYOUT_PARAMS);
-		
-        View view03 = new View(this){
-        	@Override
-        	protected void onDraw(Canvas canvas) {
-        		super.onDraw(canvas);
-        		TEXT_PAINT.setColor(Color.WHITE);
-				StaticLayout staticLayout = new StaticLayout(
-						LONG_STRING, 0, LONG_STRING.length(),
-						TEXT_PAINT, getWidth(),android.text.Layout.Alignment.ALIGN_CENTER , 
-						1.0f, 1.0f, false);
-				
-        		TEXT_PAINT.setColor(Color.WHITE);
-				canvas.drawText(LONG_STRING, 0, getHeight()/2+(staticLayout.getLineBaseline(0)-staticLayout.getLineBottom(0)/2), TEXT_PAINT);
-				TEXT_PAINT.setColor(Color.RED);
-				canvas.drawLine(0, getHeight()/2, getWidth(), getHeight()/2, TEXT_PAINT);
-				
-				TEXT_PAINT.setColor(0x7FFFFFFF&Color.GREEN);
-				canvas.drawRect(0, getHeight()/2-staticLayout.getLineBottom(0)/2, getWidth(), getHeight()/2+staticLayout.getLineBottom(0)/2, TEXT_PAINT);
 
-        	}
-        };
-        view03.setBackgroundColor(Color.GRAY);
-		linearLayout.addView(view03, LAYOUT_PARAMS);
         setContentView(linearLayout);
     }
 }
